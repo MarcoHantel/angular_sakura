b39102a246342d48a7773017047fa4675ecf4537
MarcoHantel <hantelmarco@gmailcom>
2025-06-30 13:38:34 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MainContentComponent,
+    FooterComponent,
+    ImprintComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sakura';
+}
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { landingPageComponent } from './landingPage/landingPage.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { FindUsComponent } from './find-us/find-us.component';
+
+
+@Component({
+  selector: 'app-main-content',
+  imports: [
+    landingPageComponent,
+    OurMenuComponent,
+    HowToOrderComponent,
+    FindUsComponent
+  ],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
